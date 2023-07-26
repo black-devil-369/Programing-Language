e8c310e791aade458dcb59faec3d8f187a69a3e1
black-devil-369 <ajaychauhan1262@gmail.com>
2023-07-26 08:35:41 +0530

    arrwo function

---
+++ Javascript/Advanced/arrow_function.js
@@ -0,0 +1,18 @@
+// declaring of arrow function
+// syntax: declaring_variable = (no of parameter) =>{body of the function}
+const k = (x,y) =>{
+    return x*y;
+}
+console.log(k(5,6));
+// Normal function declaring in js
+function j(x,y){
+    return x+y;
+}
+console.log(j(8,9));
+
+// setTimeout function using
+setTimeout(() => {
+    console.log("Jai Maa kali");
+},200);
+
+console.log("Jai Maa Bhavani");
