e5643829e9d66a8c7c4d1ac0d800034f9a48c3d8
black-devil-369 <ajaychauhan1262@gmail.com>
2023-08-26 19:36:57 +0530

    insertion sort using cpp

---
+++ CPP-Programing/DSA/Sorting/insertionsort.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,key,j;
+    cin>>n;
+    int arr[n];
+    for(int i =0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    for(int i =1;i<n;i++){
+        key = arr[i];
+        j = i-1;
+        while(j>=0 && arr[j]>key){
+            arr[j+1] = arr[j];
+            j = j-1;
+        }
+        arr[j+1] = key;
+    }
+    for(int i =0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
