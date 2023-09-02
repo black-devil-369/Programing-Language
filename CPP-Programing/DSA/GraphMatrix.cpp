c133f8cdfe39e0881e8558f4e9e1a394833fd78f
black-devil-369 <ajaychauhan1262@gmail.com>
2023-09-02 08:11:42 +0530

    graph matrix using cpp

---
+++ CPP-Programing/DSA/GraphMatrix.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+using namespace std;
+class Graph{
+    private:
+      int v_count;
+      int e_count;
+      int **adj;
+    public:
+      void createGraph(int,int);
+      void printmatrix();
+      void printadjacentnode(int);
+      bool isIsolated(int);
+      ~Graph();
+};
+// code for destructor for deallocating the memory
+Graph::~Graph(){
+    for(int i =0;i<v_count;i++)
+        delete []adj[i];
+    delete []adj;
+}
+// function is used to creating an graph
+void Graph::createGraph(int vno,int eno){
+    int u,v;
+    v_count = vno;
+    e_count = eno;
+    adj = new int*[v_count];
+    for(int i =0;i<v_count;i++){
+        adj[i] = new int[v_count];
+    }
+    // firstly assing 0 in all graph matrix
+    for(int i =0;i<v_count;i++){
+        for(int j = 0;j<v_count;j++){
+            adj[i][j] = 0;
+        }
+    }
+    // assing 1 in connecting node
+    for(int i =0;i<e_count;i++){
+        cout<<"\nEnter node numbers connecting edge:";
+        cin>>u>>v;
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+}
+// code for printing matrix
+void Graph::printmatrix(){
+    cout<<endl;
+    for(int i =0;i<v_count;i++){
+        for(int j =0;j<v_count;j++){
+            cout<<adj[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+// code for printing adjacentnode
+void Graph::printadjacentnode(int v){
+    if(v<v_count && v>=0){
+        for(int i=0;i<v_count;i++){
+            if(adj[v][i]==1)
+               cout<<endl<<"V"<<i;
+        }
+    }
+}
+// code for checking isoloated node
+bool Graph::isIsolated(int v)
+{
+    bool flag = true;
+    if(v<v_count && v>=0){
+        for(int i =0;i<v_count;i++){
+            if(adj[v][i]==1){
+                flag = false;
+                break;
+            }
+        }
+    }
+    return flag;
+}
+// Deriver Program
+int main(){
+    Graph g;
+    g.createGraph(5,6);
+    g.printmatrix();
+    g.printadjacentnode(1);
+    cout<<endl;
+    return 0;
+}
