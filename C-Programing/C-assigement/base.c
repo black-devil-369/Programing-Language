1c948e03ef86c7f2c85bbb7ecb8bf8dfb5874d0d
black-devil-369 <ajaychauhan1262@gmail.com>
2023-09-07 20:43:09 +0530

    question based on control statements

---
+++ C-Programing/C-assigement/base.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    printf("%%d \n");
+    printf("%%c\n");
+    printf("\\n \n");
+    printf("\\\\");
+    printf("\n");
+    int HH , MM;
+    scanf("%d %d",&HH,&MM);
+    printf("%d hour and %d Minute ",HH,MM);
+    printf("\n");
+    int x = printf("ineuron");
+    printf("%d\n",x);
+    return 0;
+}
